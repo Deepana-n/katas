025153debc84e4b1ba5ccda9b9a7a53f6d44ccea
Deepana Naidu <deepana.naidu@gmail.com>
2022-04-04 19:43:22 +0100

    Refactored RomanNumeralsToDigit class

--- src/main/java/RomanNumeralsToDigit.java
+++ src/main/java/RomanNumeralsToDigit.java
@@ -1,27 +1,31 @@
 import java.util.HashMap;
 
 public class RomanNumeralsToDigit {
+    private static final String pattern = "(^(?=[XVI])(I[XV]|V?I{0,3})$)";
+    private static final HashMap<Character,Integer> numeralMap = new HashMap<>();
 
-    public String convertRomanNumeralsToDigit(String romanNumeral){
-        HashMap<Character,Integer> numeralMap = new HashMap<>();
-        String pattern = "(^(?=[XVI])(I[XV]|V?I{0,3})$)";
-        if(!romanNumeral.matches(pattern)) return "Invalid roman numeral. Should not repeat same numeral 4x.";
+    private static HashMap<Character,Integer> createdNumeralMap(){
         numeralMap.put('I',1);
         numeralMap.put('V',5);
         numeralMap.put('X',10);
+        return numeralMap;
+    }
+
+    public String convertRomanNumeralsToDigit(String romanNumeral){
+        if(!romanNumeral.matches(pattern)) return "Invalid roman numeral. Should not repeat same numeral 4x.";
         int digit = 0;
 
        for(int i =0; i<romanNumeral.length()-1;i++){
            char cur = romanNumeral.charAt(i);
            char next = romanNumeral.charAt(i+1);
-            if(numeralMap.get(cur) < numeralMap.get(next)) {
-                digit -= numeralMap.get(cur);
+            if(createdNumeralMap().get(cur) < createdNumeralMap().get(next)) {
+                digit -= createdNumeralMap().get(cur);
             }else{
-                digit += numeralMap.get(cur);
+                digit += createdNumeralMap().get(cur);
             }
         }
 
-       int lastCharacterValue = numeralMap.get(romanNumeral.charAt(romanNumeral.length()-1));
+       int lastCharacterValue = createdNumeralMap().get(romanNumeral.charAt(romanNumeral.length()-1));
 
         return String.valueOf(digit+lastCharacterValue);
     }
