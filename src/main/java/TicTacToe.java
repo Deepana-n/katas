a6b180b1759845c0b09c18911eb1dac4ff543b87
Deepana Naidu <deepana.naidu@gmail.com>
2022-04-28 16:16:11 +0100

    Included Tic Tac Toe checker method and tests

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,2 +1,68 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class TicTacToe {
+
+    public int check(int[][] board) {
+        int countZeroes =0;
+        int countOnes =0;
+        int countTwos =0;
+        for(int row =0; row<board.length;row++){
+            for(int col=0;col<board.length;col++){
+                if(board[row][col] == 0){
+                    countZeroes++;
+                }else if(board[col][row] == 0){
+                    countZeroes++;
+                }
+                if(board[row][col] == 1) countOnes++;
+                else if(board[col][row] ==1) countOnes++;
+                else if(row==col && board[row][col]==1) countOnes++;
+                else if((row+col) == (board.length-1) && board[row][col]==1) countOnes++;
+
+                if(board[row][col] == 2) countTwos++;
+                else if(board[col][row]==2) countTwos++;
+                else if(row==col && board[row][col]==2) countTwos++;
+                else if((row+col) == (board.length-1) && board[row][col]==2) countTwos++;
+
+            }
+            if(countOnes == 3) return 1;
+            else if(countTwos==3) return 2;
+            else {
+                countOnes = 0;
+                countTwos = 0;
+            }
+
+        }
+        if(countZeroes ==9) return -1;
+
+        return -2;
+    }
+
+    public int checkDiagonal(int[][] board) {
+        int countZeroes =0;
+        int countPrincipalOnes =0;
+        int countSecondaryOnes =0;
+        int countPrincipalTwos =0;
+        int countSecondaryTwos =0;
+        for(int row =0; row<board.length;row++){
+            for(int col=0;col<board.length;col++){
+                if(row==col && board[row][col]==1) countPrincipalOnes++;
+                if((row+col) == (board.length-1) && board[row][col]==1) countSecondaryOnes++;
+
+                if(row==col && board[row][col]==2) countPrincipalTwos++;
+                if((row+col) == (board.length-1) && board[row][col]==2) countSecondaryTwos++;
+
+            }
+
+        }
+        System.out.println(countSecondaryOnes);
+        System.out.println(countPrincipalOnes);
+        if(countPrincipalOnes==3 || countSecondaryOnes==3) return 1;
+        else if(countPrincipalTwos==3 || countSecondaryTwos==3) return 2;
+        return -2;
+    }
+
+
 }
--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -1,5 +1,80 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicTacToeTest {
+
+    @Test
+    @DisplayName("This test checks for an open board")
+    public void testOpenBoard(){
+        TicTacToe ticObj = new TicTacToe();
+        int[][] board = {{0,0,0}, {0,0,0}, {0,0,0}};
+        int expected = -1;
+        int actual = ticObj.check(board);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("This test should result in player 1 winning")
+    public void testTopRowAll1(){
+        TicTacToe ticObj = new TicTacToe();
+        int[][] board = {{1,1,1}, {0,0,0}, {0,0,0}};
+        int expected = 1;
+        int actual = ticObj.check(board);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("This test should result in player 2 winning")
+    public void testSecondRowAll2(){
+        TicTacToe ticObj = new TicTacToe();
+        int[][] board = {{1,0,0}, {2,2,2}, {0,0,0}};
+        int expected = 2;
+        int actual = ticObj.check(board);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("This test should result in player 2 winning col 0")
+    public void testFirstColAll2(){
+        TicTacToe ticObj = new TicTacToe();
+        int[][] board = {{2,0,0}, {2,2,0}, {2,0,0}};
+        int expected = 2;
+        int actual = ticObj.check(board);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("This test should result in player 1 winning col 1")
+    public void testSecondColAll1(){
+        TicTacToe ticObj = new TicTacToe();
+        int[][] board = {{2,1,0}, {2,1,0}, {0,1,0}};
+        int expected = 1;
+        int actual = ticObj.check(board);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("This test should result in player 1 winning top left diagonal")
+    public void testPrincipalDiagonalTopLeftAll1(){
+        TicTacToe ticObj = new TicTacToe();
+        int[][] board = {{1,0,0}, {0,1,0}, {0,0,1}};
+        int expected = 1;
+        int actual = ticObj.checkDiagonal(board);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("This test should result in player 1 winning top left diagonal")
+    public void testSecondaryDiagonalTopRightAll1(){
+        TicTacToe ticObj = new TicTacToe();
+        int[][] board = {{0,0,1}, {0,1,0}, {1,0,0}};
+        int expected = 1;
+        int actual = ticObj.checkDiagonal(board);
+        assertEquals(expected, actual);
+    }
+
 
 }
